09730e9e1f31700652b25008b60f16c55e5bad47
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-04 01:37:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,13 +13,11 @@ end
 
 
 def add_item(title, price, quantity = 1) #opt qty = 1 :if adding item will always be @ least 1
-
-  
-    #if quantity > 1
+#if quantity > 1
       counter = 0
       while counter < quantity
         @items << title             #adds title to @array items each time a item is added
-      counter += 1                  
+      counter += 1                   #trying to say: (title * quantity) = that many titles
     end
   @total += price*quantity              #have to use += cannot reset @total to = equation
   @prev_total = @total        #tracks the @prev_total when smthg is added.. price total is total
